74358ee465f6d5d22e03b826ea35cf238e4bc815
Prasanna <prasanna.venkatesh@psiog.com>
2019-08-30 18:28:17 +0530

    Update utilities.js

--- Stylesheets/utilities.js
+++ Stylesheets/utilities.js
@@ -1,3 +1,43 @@
+function updateTimeZone(){
+  var lstCity = document.form1.lstCity;
+  timeDiff = lstCity.options[lstCity.selectedIndex].value;
+  selectedCity = lstCity.options[lstCity.selectedIndex].text;
+  updateTime();
+}
+
+function getTimeString(dateObject){
+  var timeString;
+  var hours = dateObject.getHours();
+  if(hours < 10){
+    hours = "0" + hours;
+  }
+  
+  var minutes = dateObject.getMinutes();
+  if(minutes<10){
+    minutes = "0" + minutes;
+  }
+  
+  var seconds = dateObject.getSeconds();
+  if(seconds<10){
+    seconds ="0" + seconds;
+  }
+  
+  timeString = hours + ":" + minutes + ":" + seconds;
+  return timeString;
+}
+  
+function updateTime(){
+  var nowTime = new Date();
+  var resultsText = "<p>Local Time is " + getTimeString(nowTime) + "</p>";
+  
+  nowTime.setMinutes(nowTime.getMinutes() + nowTime.getTimezoneOffset() + parseInt(timeDiff)) ;
+  
+  resultsText += "<p>" + selectedCity + " time is "+ getTimeString(nowTime) 
+  + "</p>";
+  
+  document.getElementById("ConversionResultsDIV").innerHTML = resultsText;
+}
+
 var crrncy = {'EUR': {'INR': 79.78, 'USD': 1.11}, 'USD': {'INR': 71.98, 'EUR': 0.90}}
 var btn = document.querySelector('.calculate-btn');
 var baseCurrencyInput = document.getElementById('currency-1');
@@ -47,43 +87,4 @@ var timeDiff;
 var selectedCity;
 var daylightSavingsAdjust = 0;
 
-function updateTimeZone(){
-  var lstCity = document.form1.lstCity;
-  timeDiff = lstCity.options[lstCity.selectedIndex].value;
-  selectedCity = lstCity.options[lstCity.selectedIndex].text;
-  updateTime();
-}
-
-function getTimeString(dateObject){
-  var timeString;
-  var hours = dateObject.getHours();
-  if(hours < 10){
-    hours = "0" + hours;
-  }
-  
-  var minutes = dateObject.getMinutes();
-  if(minutes<10){
-    minutes = "0" + minutes;
-  }
-  
-  var seconds = dateObject.getSeconds();
-  if(seconds<10){
-    seconds ="0" + seconds;
-  }
-  
-  timeString = hours + ":" + minutes + ":" + seconds;
-  return timeString;
-}
-  
-function updateTime(){
-  var nowTime = new Date();
-  var resultsText = "<p>Local Time is " + getTimeString(nowTime) + "</p>";
-  
-  nowTime.setMinutes(nowTime.getMinutes() + nowTime.getTimezoneOffset() + parseInt(timeDiff)) ;
-  
-  resultsText += "<p>" + selectedCity + " time is "+ getTimeString(nowTime) 
-  + "</p>";
-  
-  document.getElementById("ConversionResultsDIV").innerHTML = resultsText;
-}
 
