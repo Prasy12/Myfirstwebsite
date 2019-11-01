c846cf6365b4a89a89b6229992c122939af3aeb5
Prasanna <prasanna.venkatesh@psiog.com>
2019-11-01 09:49:41 +0530

    Update utilities.js

--- Javascript/utilities.js
+++ Javascript/utilities.js
@@ -1,3 +1,5 @@
+var timeDiff;
+var selectedCity;
 function updateTimeZone(){
   var lstCity = document.form1.lstCity;
   timeDiff = lstCity.options[lstCity.selectedIndex].value;
@@ -75,8 +77,7 @@ function convertCurrency(event) {
 }
 
 btn.addEventListener('click', convertCurrency);
-var timeDiff;
-var selectedCity;
-var daylightSavingsAdjust = 0;
+
+
 
 
